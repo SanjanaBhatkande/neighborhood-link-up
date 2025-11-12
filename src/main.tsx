77265d9245b1ed19dea36c7484e996e89480997c
Sanjana Bhatkande <sanjanagbhatkande@gmail.com>
2025-11-12 11:02:19 +0530

    Latest changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./setupLeaflet";
-import "leaflet/dist/leaflet.css";
+
 
 
 createRoot(document.getElementById("root")!).render(<App />);
